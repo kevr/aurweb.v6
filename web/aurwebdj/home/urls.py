c35b60525887abfcdb1aeac47a7a7057a586255f
Kevin Morris <kevr.gtalk@gmail.com>
2019-02-22 09:23:03 -0800

    Add a 'home' app for the django project
    home is responsible for routing and rendering views related to
    portal/index of aur.archlinux.org
    
    Signed-off-by: Kevin Morris <kevr.gtalk@gmail.com>

---
+++ web/aurwebdj/home/urls.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from django.urls import path
+
+urlpatterns = [
+  # path("accounts/", UserSearchView.as_view(), name="user_search"),
+  # path("account/<user>/comments/", UserCommentsView.as_view(), name="user_comments"),
+  # account/<user>/update/ may be removed in favor of GET+POST account/<user>/edit/
+  # path("account/<user>/update/" UserUpdateView.as_view(), name="user_update"),
+  # path("account/<user>/edit/", UserEditView.as_view(), name="user_edit"),
+  # path("account/<user>/", UserView.as_view(), name="user"),
+  # path("tu/", TrustedUserView.as_view(), name="tu"),
+]
+
+
