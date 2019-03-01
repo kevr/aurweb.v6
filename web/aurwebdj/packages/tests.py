70c3cc7081a55117b10d3987ff3240ed73d74228
Kevin Morris <kevr.gtalk@gmail.com>
2019-03-01 04:58:10 -0800

    Add packages.tests
    Signed-off-by: Kevin Morris <kevr.gtalk@gmail.com>

--- web/aurwebdj/packages/tests.py
+++ web/aurwebdj/packages/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import TestCase, TransactionTestCase
+
+from packages.models import *
+
+class PackagesViewTest(TestCase):
+
+  def test_index(self):
+    response = self.client.get("/packages/")
+    self.assertEqual(response.status_code, 200)
+
+  def test_o(self):
+    response = self.client.get("/packages/?O=0")
+    self.assertEqual(response.status_code, 200)
 
 # Create your tests here.
